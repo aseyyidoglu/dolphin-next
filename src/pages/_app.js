129d36ca07680e4fe157c8bbb224f7849208dc0d
Ali Seyyidoglu <ali.seyyidoglu@gmail.com>
2023-04-22 20:59:31 +1000

    typo fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-import { ThemeProvider } from '../context/theme.js'
+import { ThemeProvider } from '@/context/theme.js'
 import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
 import '@/styles/globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
@@ -16,3 +17,4 @@ export default function App({ Component, pageProps }) {
 
 // @Ali: Need to change the way ThemeProvider asyncronously updates the app page
 // currently this is causing HTML - React hydration error, 2023-04-02 02:33AM
+// Remove font settings from App and keep them in global style
